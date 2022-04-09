97c9313d740de4944c4989dee26ac93f82ec3f18
Hoang Dang <nghiemdanghoang@gmail.com>
2022-04-09 07:41:35 +0700

    setup redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,37 @@
 import { configureStore, Action } from '@reduxjs/toolkit'
 import { ThunkAction } from 'redux-thunk'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
+import storage from 'redux-persist/lib/storage'
 import rootReducer, { RootState } from './rootReducer'
 
-const store = configureStore({
-  reducer: rootReducer
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+
+export default persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 
 export type AppThunk = ThunkAction<void, RootState, null, Action<string>>
-
-export default store
